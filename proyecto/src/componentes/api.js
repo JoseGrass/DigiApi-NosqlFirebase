50de9fb67ac7eee1f8d381f108743c0785fcf02d
JoseGrass <122506998+JoseGrass@users.noreply.github.com>
2025-05-26 16:37:38 +0000

    commit1

---
+++ proyecto/src/componentes/api.js
@@ -0,0 +1,11 @@
+async function  conexionLista() {
+    const res = await fetch('https://digimon-api.vercel.app/api/digimon')
+    const data = await res.json()
+    return data;
+}
+
+async function General() {
+    app.innerHTML = "";
+    const infoDigimones = await conexionLista();
+    mostrarLista(infoDigimones)
+}
